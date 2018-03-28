b31e011b0e43fe199c0615348f66319b693398ef
CskCnklc <cskcnklc@gmail.com>
2018-03-28 15:08:24 +0100

    change file locations and rename

--- backend/controllers/something.js
+++ backend/controllers/something.js
@@ -1,14 +1,6 @@
 'use strict';
 
-const pg = require('pg-promise')();
-
-const db = pg({
-	host:     'localhost',
-	port:     process.env.TRAVELOGUE_DB_PORT,
-	database: process.env.TRAVELOGUE_DB_NAME,
-	user:     process.env.TRAVELOGUE_DB_USER,
-	password: process.env.TRAVELOGUE_DB_PASS,
-});
+const db = require('../configs/db.js');
 
 function something (thing) {
 	const sql = `
@@ -25,4 +17,4 @@ WHERE thing = $1`;
 		});
 }
 
-module.exports.something = something;
+module.exports= {something};
