afa5322d97aeb183ed51d8c27b29b9738db2c63c
Scott Martin <scott+school@scottmartin.systems>
2018-03-29 12:14:33 +0100

    Yet more ESlint

--- backend/routers/rootRouter.js
+++ backend/routers/rootRouter.js
@@ -1,7 +1,12 @@
 const rootRouter = require('express').Router();
 const apiRouter = require('./apiRouter');
 
-rootRouter.get('/',(req,res)=> res.status(200).sendFile('index.html',{root:'./backend/static'}));
-rootRouter.use('/api',apiRouter);
+rootRouter.get('/', (req, res) => {
+	res.status(200).sendFile(
+		'index.html', { root: './backend/static' }
+	);
+});
 
-module.exports= rootRouter
+rootRouter.use('/api', apiRouter);
+
+module.exports = rootRouter;
