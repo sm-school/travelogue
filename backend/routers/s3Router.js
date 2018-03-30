e297131fa2d5bbf0d8ca49b91f6481b9ec22f426
Scott Martin <scott+school@scottmartin.systems>
2018-03-30 17:34:12 +0100

    Code style fixes

--- backend/routers/s3Router.js
+++ backend/routers/s3Router.js
@@ -2,8 +2,8 @@
 
 const s3Router = require('express').Router();
 
-const {getSignatureS3} = require('../controllers/s3');
+const { getSignatureS3 } = require('../controllers/s3');
 
 s3Router.get('/sign-s3', getSignatureS3);
 
-module.exports = s3Router; 
+module.exports = s3Router;
