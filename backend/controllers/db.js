0af785a4d28b2677fa7487f0b1467cac975be00b
Scott Martin <scott+school@scottmartin.systems>
2018-04-01 00:24:33 +0100

    Add metadataRequired function for API

--- backend/controllers/db.js
+++ backend/controllers/db.js
@@ -5,4 +5,27 @@ const env = require('../configs/env');
 
 const db = pg(env.DB_URL);
 
+db.connect()
+	.then(obj => {
+		obj.done();
+	})
+	.catch(error => {
+		console.log(error.message || error);
+	});
+
+function metadataRequired (userId) {
+	const sql = `
+	SELECT s3_id FROM images
+	WHERE user_id = $1
+	AND metadata_required = TRUE`;
+
+	return db.any(sql, userId)
+		.then( data => {
+			return data;
+		})
+		.catch( error => {
+			console.log({ error: error.message });
+		});
+}
+
 module.exports = db;
