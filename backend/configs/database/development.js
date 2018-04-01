2d850de9db39038c5500b1c8574685987acaf0ee
Scott Martin <scott+school@scottmartin.systems>
2018-04-01 14:24:06 +0100

    Set database names for dev and test; get dev db pass from env

--- backend/configs/database/development.js
+++ backend/configs/database/development.js
@@ -1,9 +1,14 @@
 'use strict';
 
-const username = 'admin';
-const password = 'postgres';
+const username = 'travelogue';
+const password = process.env.TRAVELOGUE_DB_PASS;
 const port = 5432;
 const db_name = 'dev';
+
+if (!password) {
+	throw new Error('Can\'t start Travelogue: set TRAVELOGUE_DB_PASS in env');
+}
+
 module.exports = {
 	DB_URL: `postgres://${username}:${password}@localhost:${port}/${db_name}`,
 };
--- backend/configs/database/test.js
+++ backend/configs/database/test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const username = 'admin';
-const password = 'postgres';
+const username = 'travelogue';
+const password = 'test db password';
 const port = 5432;
-const db_name = 'test';
+const db_name = 'travelogue_test';
 
 module.exports = {
 	DB_URL: `postgres://${username}:${password}@localhost:${port}/${db_name}`,
