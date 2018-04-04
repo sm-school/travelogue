ba9b72911afbfc3614164071f5511999a9946aa1
Scott Martin <scott+school@scottmartin.systems>
2018-04-04 22:04:43 +0100

    Sigh

---
+++ frontend/tests/actions.test.js
@@ -0,0 +1,8 @@
+import * as actions from '../src/actions';
+import * as types from '../src/constants/action-types';
+
+describe('something', () => {
+	it('should something', () => {
+		expect(1).toEqual(1);
+	});
+});
