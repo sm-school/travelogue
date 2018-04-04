e6182e4adf6669e00e533e77c5beb859229d1603
CskCnklc <cskcnklc@gmail.com>
2018-04-04 11:08:28 +0100

    Clean root.js

--- backend/routers/root.js
+++ backend/routers/root.js
@@ -1,6 +1,5 @@
 const rootRouter = require('express').Router();
 const apiRouter = require('./api');
-const authRouter = require('./auth');
 const isLoggedIn = require('../helpers/isLoggedIn')
 const {sendIndexHtml}= require('../controllers/react')
 rootRouter.use('/api', apiRouter);
