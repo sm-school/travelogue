986dce144cf7c29a69681e613a08ed0347ae634b
CskCnklc <cskcnklc@gmail.com>
2018-04-04 11:13:56 +0100

    keep main.js

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Main = (props)=> {
+	return (
+		<div>
+			<nav className="navbar">
+				<div className="navbar-header">
+					<Link className="navbar-brand" to="/">Travelogue</Link>
+				</div>
+						
+				<ul className="navbar-link">
+					<li><NavLink to="/">Home</NavLink></li>
+					<li><NavLink to="/login">Login</NavLink></li>
+					<li><NavLink to="/register">Register</NavLink></li>
+				</ul>
+					
+			</nav>
+			<div className="container">
+				{props.children}
+			</div>
+		</div>
+	);
+};
+
+Main.propTypes = {
+	children: PropTypes.array,
+};
+export default Main;
