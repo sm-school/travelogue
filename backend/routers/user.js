1f7f44159e02e7a4090c8adc5717465cce181338
Scott Martin <scott+school@scottmartin.systems>
2018-04-05 18:56:36 +0100

    Tidy

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,10 +1,34 @@
-'use strict'
-const {registerUser, sendUserData} = require('../controllers/user')
-const isLoggedIn = require('../helpers/isLoggedIn');
+'use strict';
+
 const userRouter = require('express').Router();
 const passport = require('../passport');
 
-userRouter.post('/register',registerUser,passport.authenticate('local', { session: true }), sendUserData);
-userRouter.post('/login',passport.authenticate('local', { session: true }),sendUserData)
-userRouter.get('/check',isLoggedIn,sendUserData)
+const { registerUser, sendUserData } = require('../controllers/user');
+const isLoggedIn = require('../helpers/isLoggedIn');
+
+userRouter.post(
+	'/register',
+	registerUser,
+	passport.authenticate(
+		'local',
+		{ session: true }
+	),
+	sendUserData
+);
+
+userRouter.post(
+	'/login',
+	passport.authenticate(
+		'local',
+		{ session: true }
+	),
+	sendUserData
+);
+
+userRouter.get(
+	'/check',
+	isLoggedIn,
+	sendUserData
+);
+
 module.exports = userRouter;
