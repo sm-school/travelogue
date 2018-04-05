74ccda940360634666e8d7bfaf9e124ba194a63a
Scott Martin <scott+school@scottmartin.systems>
2018-04-05 16:05:43 +0100

    fetchUser has changed to fetchUsername in actions

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
 
 	componentDidMount() {
 		if (Object.keys(this.props.user).length == 0) {
-			this.props.fetchUser();
+			this.props.fetchUsername();
 		}
 	}
 	render() {
@@ -46,7 +46,7 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-	fetchUser : PropTypes.func,
+	fetchUsername : PropTypes.func,
 	user: PropTypes.object,
 };
 export default App;
