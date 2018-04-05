493da41bc02735c3ba114fe25eb9a5e19713580b
Scott Martin <scott+school@scottmartin.systems>
2018-04-05 16:01:35 +0100

    Add PropTypes

--- frontend/src/components/UserMenu.js
+++ frontend/src/components/UserMenu.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const UserMenu = props =>{
 	return (<h1>{props.user.username}</h1>);
+};
+
+UserMenu.propTypes = {
+	user: PropTypes.object,
 };
 
 export default UserMenu;
