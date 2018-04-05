5d9ea40b01213c0539e3ec85b237fcdf098b573e
Scott Martin <scott+school@scottmartin.systems>
2018-04-05 18:50:39 +0100

    Tidy

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,40 +1,53 @@
 const bcrypt = require('bcrypt');
-const salt = 10;
+
 const db = require('../controllers/db').db;
-function hashing(password) {
+
+const salt = 10;
+
+function hashing (password) {
 	return bcrypt.hash(password, salt);
 }
 
-function registerUser(req, res,next) {
+function registerUser (req, res, next) {
 	saveUser(req.body)
-		.then(function(_d) {
+		.then( _d => {
 			next();
 		})
-		.catch(function(err) {
-            console.log(err);
-			res.status(400).json(err);
+		.catch( error => {
+			console.log(error);
+			res.status(400).json(error);
 		});
 }
+
 // helper function to work with user
 
-function saveUser(user) {
-    console.log(user);
-	return hashing(user.password).then(function(hash) {
-		return db.none('INSERT INTO account (username, pass) VALUES ($1,$2)', [user.username, hash]);
-	});
+function saveUser (user) {
+	console.log(user);
+	return hashing(user.password)
+		.then( hash => {
+			return db.none(
+				'INSERT INTO account (username, pass) VALUES ($1,$2)',
+				[ user.username, hash ]
+			);
+		});
 }
 
-function getUserByUsername(username) {
-	return db.one('SELECT * FROM account WHERE username = $1', [username]);
+function getUserByUsername (username) {
+	return db.one(
+		'SELECT * FROM account WHERE username = $1',
+		[ username ]
+	);
 }
 
-function getUserById(id) {
-	return db.one('SELECT * FROM account WHERE id = $1', [id]);
+function getUserById (id) {
+	return db.one(
+		'SELECT * FROM account WHERE id = $1',
+		[ id ]
+	);
 }
 
+const sendUserData = (req, res) => {
+	res.status(200).json({ user: { username:req.user.username } });
+};
 
-const sendUserData = (req,res)=>{
-	res.status(200).json({user:{username:req.user.username}});
-}
-
-module.exports={getUserById,getUserByUsername,registerUser,sendUserData}
+module.exports = { getUserById, getUserByUsername, registerUser, sendUserData };
