4c7377db951b6059bbf84db053fcc0f626f940c2
Scott Martin <scott+school@scottmartin.systems>
2018-04-06 12:34:06 +0100

    Fix bug in call to Vision API

--- backend/controllers/metadataApis.js
+++ backend/controllers/metadataApis.js
@@ -3,26 +3,25 @@
 const getWikipediaData = require('./apis/wikipedia.js');
 const getVisionData = require('./apis/vision.js');
 
-// Test data: 'parliament', 'pisa', no landmarks: 'dnvb7a'
-
 function fetchMetadata (imageId) {
-	return getVisionData(imageId)
-		.then ( imageData => {
-			return getLandmarks(imageData)
-				.then( wikiData => {
-					wikiData.forEach( landmark => {
-						for (let i = 0; i < wikiData.length; i++) {
-							imageData.landmarks[i].page = wikiData[i].page;
-							imageData.landmarks[i].extract = wikiData[i].extract;
-						}
-					});
+	let imageData;
 
-					return imageData;
-				});
+	return getVisionData(imageId)
+		.then( visionData => {
+			imageData = JSON.parse(JSON.stringify(visionData));
+			return getLandmarks(visionData);
 		})
-		.catch( error => {
-			console.log(error);
-		});
+		.then( wikiData => {
+			wikiData.forEach( landmark => {
+				for (let i = 0; i < wikiData.length; i++) {
+					imageData.landmarks[i].page = wikiData[i].page;
+					imageData.landmarks[i].extract = wikiData[i].extract;
+				}
+			});
+
+			return imageData || {};
+		})
+		.catch( error => console.log(error) );
 }
 
 function getLandmarks (imageData) {
