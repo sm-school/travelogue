f7805a8dd66810d228e818f7b843370f7eeae47d
Scott Martin <scott+school@scottmartin.systems>
2018-04-07 22:50:01 +0100

    Tidy

--- backend/passport.js
+++ backend/passport.js
@@ -1,95 +1,89 @@
+'use strict';
+
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const {getUserByEmail,getUserById,getUserByOauth} = require('./controllers/user');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-// configure passport to use local strategy
-// that is use locally stored credentials
+const { getUserByEmail, getUserById, getUserByOauth } = require('./controllers/user');
+
+// Local credentials
 passport.use(
 	new LocalStrategy({
-		usernameField: 'email'
-	  }, function(email, password, done) {
-		console.log(email,password)
+		usernameField: 'email',
+	}, (email, password, done) => {
 		return getUserByEmail(email)
-			.then(function(user) {
-				if(!bcrypt.compareSync(password,user.pass)) return done(null, false);
+			.then( user => {
+				if (!bcrypt.compareSync(password, user.pass)) {
+					return done(null, false);
+				} else {
 					return done(null, user);
-				})
-			.catch(function(err) {
+				}
+			})
+			.catch( err => {
 				console.log(err);
 				return done(null, false);
 			});
 	})
 );
 
-// google passport authentication strategy
+// Google account credentials
 passport.use(
 	new GoogleStrategy({
 		clientID: process.env.GOOGLE_CLIENT_ID,
 		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		callbackURL:"/api/auth/google/callback"
-	}, function(accessToken,refreshToken,profile,done) {
-		
+		callbackURL: '/api/auth/google/callback',
+	}, (accessToken, refreshToken, profile, done) => {
 		let userOauth = userDetailsOauth(profile);
-			return getUserByOauth(userOauth)
-				.then(function(user){
-				
-					console.log('you are in');
-					return done(null, user);
-				})
-				.catch(function(err) {
-				
-					console.log(err);
-					return done(null, false);
-				});
-			
-	}));
 
+		return getUserByOauth(userOauth)
+			.then( user => {
+				return done(null, user);
+			})
+			.catch( err => {
+				console.log(err);
+				return done(null, false);
+			});
+	})
+);
 
 const userDetailsOauth = (user) => {
 	let userOauth = {};
 
-	if(user.provider = "google") {
+	if (user.provider = 'google') {
 		userOauth.id = user.id;
 		userOauth.displayName = user.displayName;
-	
+
 		userOauth.gmail_sign_in = 'TRUE';
 		userOauth.password = user.id;
 
-		if(user.name != undefined){
+		if (user.name != undefined) {
 			userOauth.firstName = user.name.givenName;
 			userOauth.lastName = user.name.familyName;
 		}
-		if(user.emails != undefined){
+		if (user.emails != undefined) {
 			userOauth.email = user.emails[0].value;
 		}
-		if(user.photos != undefined){
+		if (user.photos != undefined) {
 			userOauth.photo = user.photos[0].value;
 		}
-		
-		if(user._json != undefined){
+		if (user._json != undefined) {
 			userOauth.domain = user._json.domain;
 		}
 	}
 
 	return userOauth;
-	
-}
+};
 
-
-	// serialise user into session
-passport.serializeUser(function(user, done) {
+passport.serializeUser( (user, done) => {
 	done(null, user.id);
 });
 
-// deserialise user from session
-passport.deserializeUser(function(id, done) {
-	getUserById(id).then(function(user) {
-		done(null, user);
-	});
+passport.deserializeUser( (id, done) => {
+	getUserById(id)
+		.then( user => {
+			done(null, user);
+		});
 });
 
-
-
-module.exports= passport;
+module.exports = passport;
