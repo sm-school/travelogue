96f769df23049f72e57c1d34d95c2f5df3c46790
Scott Martin <scott+school@scottmartin.systems>
2018-04-07 22:07:41 +0100

    Be succinct

--- backend/routers/api.js
+++ backend/routers/api.js
@@ -2,15 +2,14 @@
 
 const api = require('express').Router();
 
-const authRouter = require('./auth');
-const userRouter = require('./user');
-const imageRouter = require('./image');
+const auth = require('./auth');
+const image = require('./image');
+const s3 = require('../controllers/s3');
+const user = require('./user');
 
-const getSignatureS3 = require('../controllers/s3');
-
-api.use('/auth', authRouter);
-api.use('/image', imageRouter);
-api.get('/sign-s3', getSignatureS3);
-api.use('/user', userRouter);
+api.use('/auth', auth);
+api.use('/image', image);
+api.get('/sign-s3', s3);
+api.use('/user', user);
 
 module.exports = api;
