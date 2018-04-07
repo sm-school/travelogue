c1565cf46152b0047d74b9351542c057b998da40
Scott Martin <scott+school@scottmartin.systems>
2018-04-07 18:05:11 +0100

    Tidy component source; add a fleuron next to brand in navigation

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import '../styles/components/Navigation.scss';
 import PropTypes from 'prop-types';
+
 import UserMenuContainer from '../containers/UserMenuContainer';
+
+import '../styles/components/Navigation.scss';
+
 class Navigation extends React.Component {
 	componentDidMount() {
 		window.addEventListener('click', this.props.turnMenuOff);
 	}
+
 	componentWillUnmount() {
 		window.removeEventListener('click', this.props.turnMenuOff);
 	}
+
 	renderNavLinks = user =>{
 		if (user.loggedIn) {
-			return ( <li><UserMenuContainer/></li> 
-			);
+			return ( <li><UserMenuContainer/></li> );
 		} else {
-			return [ <li key='login'><NavLink to="/login">Login</NavLink></li>,
-				<li key='register'><NavLink to="/register">Register</NavLink></li> ];
+			return [
+				<li key='login'><NavLink to="/login">Login</NavLink></li>,
+				<li key='register'><NavLink to="/register">Register</NavLink></li>,
+			];
 		}
 	};
+
 	render() {
-		return (<nav className="navigation">
-			<div className="navigation_link_wrapper">
-				<Link className="navigation__brand" to={ this.props.user.loggedIn ? '/dashboard' : '/'}>Travelogue</Link>
-				<ul className="navigation__ul">
-					{this.renderNavLinks(this.props.user)} 
-				
-				</ul>
-			</div>
-		</nav>);
+		return (
+			<nav className="navigation">
+				<div className="navigation_link_wrapper">
+					<Link className="navigation__brand" to={ this.props.user.loggedIn ? '/dashboard' : '/'}>Travelogue</Link>
+					<ul className="navigation__ul">
+						{this.renderNavLinks(this.props.user)}
+					</ul>
+				</div>
+			</nav>
+		);
 	}
 };
 
