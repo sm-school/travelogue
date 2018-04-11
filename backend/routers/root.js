a94744b955170d0e091d2b2cef5a01b05e2ca381
CskCnklc <cskcnklc@gmail.com>
2018-04-11 23:20:42 +0100

    domain verification

--- backend/routers/root.js
+++ backend/routers/root.js
@@ -11,6 +11,14 @@ rootRouter.use('/api', apiRouter);
 
 rootRouter.get([ '/', '/login', '/logout', '/register' ], sendIndexHtml);
 
+rootRouter.get( '/.well-known/acme-challenge/RCLTT3G4dOYLF8bIMi2Sy-0iu7MMljhOYFkcSmQcRnI', (req,res)=>{
+    res.status(200).sendFile('sslcheck', { root: './backend/static' });
+});
+rootRouter.get( '/googlefc6baf798576a6fd.html ', (req,res)=>{
+    res.status(200).sendFile('googlefc6baf798576a6fd.html', { root: './backend/static' });
+});
+
+
 rootRouter.get('/*', isLoggedIn, sendIndexHtml);
 
 module.exports = rootRouter;
